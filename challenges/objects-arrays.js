53428af99a724c4fff7fc02f2cf2bf3eec4b74fe
denmercs <denmercs@gmail.com>
2019-06-14 11:42:15 -0500

    made some changes in objects-array problem 3

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -88,7 +88,9 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = []
   for(let i = 0; i < graduates.length; i++) {
-    uni.push(graduates[i].university.match(/Uni/));
+    if(graduates[i].university.match(/Uni/)) {
+      uni.push(graduates[i]);
+    }
   }
 console.log(uni);
 
